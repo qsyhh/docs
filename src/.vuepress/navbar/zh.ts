80e67c3327b693a971da4b8f8b153c09e61d506d
SHIKEAIXY <zsfys225011250@qq.com>
2024-08-18 01:02:28 +0800

    UP

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,7 +1,10 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  { text: '介绍', link: '/README.md', icon: 'creative' },
-  { text: '系统目录', link: '/docs/README.md', icon: 'customize' },
-  { text: '打赏', link: '/sponsor.md', icon: 'flower' },
+
+   { text: '介绍', link: '/README.md', icon: 'ellipsis' },
+   { text: "选择所需要系统", icon: "lightbulb", prefix: "/docs",
+   children: [ { text: "Windows", icon: "lightbulb", prefix: "windows/", children: [{ text: "windows安装", icon: "ellipsis", link: "README.md" }], },
+   { text: "Linux", icon: "lightbulb", prefix: "linux/", children: [{ text: "Linux安装", icon: "ellipsis", link: "README.md" }], }, ], },
+   { text: '打赏', link: '/sponsor.md', icon: 'ellipsis' },
 ]);
