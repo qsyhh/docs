e21959771ad513e270bbad72110d25788da0f73d
SHIKEAIXY <zsfys225011250@qq.com>
2024-08-21 21:04:28 +0800

    fix:icon

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -2,10 +2,10 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
 
-   { text: '介绍', link: '/README.md', icon: 'ellipsis' },
-   { text: "选择所需要系统", icon: "lightbulb", prefix: "/docs",
-   children: [ { text: "Windows", icon: "lightbulb", prefix: "windows/", children: [{ text: "windows安装", icon: "ellipsis", link: "README.md" }], },
-   { text: "Linux Ubuntu", icon: "lightbulb", prefix: "/", children: [{ text: "Linux服务器安装", icon: "ellipsis", link: "linux/README.md" },{ text: "Linux桌面版安装", icon: "ellipsis", link: "linux2/ README.md" }], }, ], },
-   { text: '问题合集', link: '/ISS.md', icon: 'ellipsis' },
-   { text: '打赏', link: '/sponsor.md', icon: 'ellipsis' },
+   { text: '介绍', link: '/README.md', icon: 'lightbulb' },
+   { text: "选择所需要系统", icon: "computer", prefix: "/docs",
+   children: [ { text: "Windows", prefix: "windows/", children: [{ text: "windows安装", icon: "desktop", link: "README.md" }], },
+   { text: "Linux Ubuntu", prefix: "./", children: [{ text: "Linux服务器安装", icon: "server", link: "linux/README.md" },{ text: "Linux桌面版安装", icon: "desktop", link: "linux2/ README.md" }], }, ], },
+   { text: '问题合集', link: '/ISS.md', icon: 'pen-to-square' },
+   { text: '打赏', link: '/sponsor.md', icon: 'heart' },
 ]);
