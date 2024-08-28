a998800a90f3a80ea3fa3a3af69953e54d188517
SHIKEAIXY <zsfys225011250@qq.com>
2024-08-28 10:58:59 +0800

    UP

--- push.bat
+++ push.bat
@@ -1,3 +1,16 @@
+@echo off
 git add .
 git commit -m UP
 git push
+REM 推送失败
+if ERRORLEVEL 1 (
+echo git push 失败。是否要使用 git push -f 强制推送？ (y/n)
+set /p response=
+if /i "%response%"=="y" (
+git push -f
+  ) else (
+echo 未执行强制推送。
+  )
+) else (
+echo git push 成功。
+)
